e0cc81137fc61574822691de798f153ca4f84264
ganiamadeus <gani468@gmail.com>
2020-02-24 22:59:46 +0530

    ramges

---
+++ ranges.py
@@ -0,0 +1,31 @@
+#sequence representing a arithmetic progression of integres
+
+for i in range(5):
+    print(i)#0,1,2,3,4
+
+
+for i in range(5,10):
+    print(i)#
+
+lst=list(range(10,15))
+
+for i in lst:
+    print(i)
+
+lst_one=list(range(0,10,2))
+
+# range(stop).
+# range(start, stop).
+# rnage(start, stop, step).
+# range doesn't support keyword argument.
+r=10
+
+for i in range(r):
+    print(i)
+
+
+g=[1,2,3,4,5]
+
+for i in enumerate(g):#returns tuple (index, value)
+    print(i)
+
