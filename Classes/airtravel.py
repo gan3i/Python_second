32f3d6ceca813ea9dc918916fb675de69b96c6ec
ganiamadeus <gani468@gmail.com>
2020-03-02 17:31:55 +0530

    booking

--- Classes/airtravel.py
+++ Classes/airtravel.py
@@ -1,5 +1,5 @@
 
-"""MOdel for aircraft flights"""
+"""Model for aircraft flights"""
 
 class Flights:
     """ A flight with perticular passenger aircraft"""
@@ -15,6 +15,8 @@ class Flights:
         self._number=number#implementation details of an obejct which are not ment for consuption
         #or manipulattion by clinets of the object should be pre-fixed with underscore, it's a widely used convention.
         self._aircraft= aircraft
+        rows, seats = self._aircraft.seating_plan()
+        self._seating = [{letter:None for letter in seats} for _ in rows] 
 
     
     def number(self):
@@ -29,6 +31,53 @@ class Flights:
         return self._aircraft.model()
 
 
+    def allocate_seat(self,seat, passenger):
+        """Allocate a seat to a passenger
+
+        args:
+            seat: a seat designator such as "12c" or "12f"
+            passenger : name of the passenger
+
+        raises :
+            valueerror: is the given seat is unavailable"
+        """
+        rows,seat_letter = self._aircraft.seating_plan()
+        row,letter = self._parse_seat(seat)
+        if self._seating[row][row_letter] is not None:
+            raise ValueError(f"seat {seat} has already been occupied")
+
+        self._seating[row][row_letter] = passenger
+
+
+    def _parse_seat(set):
+        row_letter = seat[-1]
+        if row_letter not in seat_letter:
+            raise ValueError(f"Invalid seat letter {row_letter}")
+
+        row_text = seat[:-1]
+        try:
+            row = int(row_text)
+        except ValueError:
+            raise ValueError(f"invalid seat row {row_text}")
+
+        if row not in rows:
+            raise ValueError(f"Invalid row number {row}")
+
+
+    def relocate_passenger(self, from_seat,to_seat):
+        """relocate a passnger to a diffrent seat
+
+            args:
+                from_seat: the existing seat designation of the 
+                        passenger to be moved.
+                to_seat : the new seat designator.
+
+        """
+        from_row, from_letter = self._parse_seat(from_seat)
+
+        
+
+
 class Aircraft:
 
     def __init__(self, registartion, model, num_rows, num_seats_per_row):
@@ -39,7 +88,7 @@ class Aircraft:
 
 
     def registartion(self):
-        return self._registartion
+        return self._registration
 
 
     def model(self):
@@ -55,7 +104,11 @@ class Aircraft:
 
 
     def seating_plan(self):
+        """Returns the seating plan given number of rows and letter"""
         return (range(1,self._num_rows+1),
                 "ABCDEFGHJK"[:self._num_seats_per_row])
+
+
+
 
 
--- Classes/booking.py
+++ Classes/booking.py
@@ -1,4 +1,5 @@
 from airtravel import Flights, Aircraft
+from pprint import pprint as pp
 
 a = Aircraft("E-GUP","Airbus A319",num_rows=22,num_seats_per_row=4)
 
@@ -7,3 +8,14 @@ print(a.seating_plan())
 objf=Flights("SN99999",Aircraft("E-GUP","Airbus A319",num_rows=22,num_seats_per_row=4))
 
 print(objf.aircraft_model())
+
+print(objf._seating[12]["A"])
+
+objf.allocate_seat("12A","gani")
+objf.allocate_seat("12A","gani")
+# objf.allocate_seat("12A","django")
+
+print(objf._seating[12]["A"])
+
+
+# pp(objf._seating)
