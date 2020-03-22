990e665bd83633ecb33f313b46fbf98319094349
ganiamadeus <gani468@gmail.com>
2020-03-22 22:36:08 +0530

    fixture

--- UnitTesting/PyTest/test_phonepytest.py
+++ UnitTesting/PyTest/test_phonepytest.py
@@ -3,15 +3,37 @@
 class PhoneBook:
 
     def __init__(self) ->None:
-        self.number =None
+        self.numbers ={}
 
     def add(self,name , number):
-        self.number = number
+        self.numbers[name] = number
 
     def lookup(self , name):
-        return self.number
+        return self.numbers[name]
 
-def test_lookup_by_name():
+    def names(self):
+        return set(self.numbers.keys())
+
+# pytest way for setting up test fixtures/setup 
+@pytest.fixture
+def phonebook():
     phonebook = PhoneBook()
+    return phonebook
+
+#dependencies are injected
+def test_lookup_by_name(phonebook):
     phonebook.add("Bob","1234")
     assert "1234" == phonebook.lookup("Bob")
+
+def test_phonebook_contains_all_names(phonebook):
+    phonebook.add("Bob","1234")
+    phonebook.add("Missing","12345")
+    assert phonebook.names() == {"Bob","Missing"}
+    assert "Missing" in phonebook.names()
+
+import pytest
+def test_phonbook_missing_name_raises_error(phonebook):
+    with pytest.raises(KeyError):
+        phonebook.lookup("Bob")
+
+
